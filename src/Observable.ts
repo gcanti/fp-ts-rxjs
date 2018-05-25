60f060b6cebf82fc6c0177b2c229ddc495a70a59
Marcin Wadon <marcin@wadon.net>
2018-05-25 18:28:53 +0200

    support rxjs v6

--- src/Observable.ts
+++ src/Observable.ts
@@ -1,15 +1,10 @@
-import { Monoid } from 'fp-ts/lib/Monoid'
-import { Monad1 } from 'fp-ts/lib/Monad'
 import { Alternative1 } from 'fp-ts/lib/Alternative'
-import { Observable } from 'rxjs/Observable'
-import 'rxjs/add/observable/empty'
-import 'rxjs/add/observable/merge'
-import 'rxjs/add/observable/of'
-import 'rxjs/add/observable/combineLatest'
-import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/mergeMap'
+import { Monad1 } from 'fp-ts/lib/Monad'
+import { Monoid } from 'fp-ts/lib/Monoid'
+import { combineLatest, EMPTY, merge, Observable, of as rxOf } from 'rxjs'
+import { map as rxMap, mergeMap } from 'rxjs/operators'
 
-declare module 'rxjs/Observable' {
+declare module 'rxjs/internal/Observable' {
   interface Observable<T> {
     _URI: URI
     _A: T
@@ -28,23 +23,23 @@ export type URI = typeof URI
 
 export const getMonoid = <A = never>(): Monoid<Observable<A>> => {
   return {
-    concat: (x, y) => Observable.merge(x, y),
-    empty: Observable.empty()
+    concat: (x, y) => merge(x, y),
+    empty: EMPTY
   }
 }
 
-const map = <A, B>(fa: Observable<A>, f: (a: A) => B): Observable<B> => fa.map(f)
+const map = <A, B>(fa: Observable<A>, f: (a: A) => B): Observable<B> => fa.pipe(rxMap(f))
 
-const of = <A>(a: A): Observable<A> => Observable.of(a)
+const of = <A>(a: A): Observable<A> => rxOf(a)
 
 const ap = <A, B>(fab: Observable<(a: A) => B>, fa: Observable<A>): Observable<B> =>
-  Observable.combineLatest(fab, fa, (f, a) => f(a))
+  combineLatest(fab, fa, (f, a) => f(a))
 
-const chain = <A, B>(fa: Observable<A>, f: (a: A) => Observable<B>): Observable<B> => fa.flatMap(f)
+const chain = <A, B>(fa: Observable<A>, f: (a: A) => Observable<B>): Observable<B> => fa.pipe(mergeMap(f))
 
-const alt = <A>(x: Observable<A>, y: Observable<A>): Observable<A> => Observable.merge(x, y)
+const alt = <A>(x: Observable<A>, y: Observable<A>): Observable<A> => merge(x, y)
 
-const zero = <A>(): Observable<A> => Observable.empty()
+const zero = <A>(): Observable<A> => EMPTY
 
 export const observable: Monad1<URI> & Alternative1<URI> = {
   URI,
--- test/Observable.ts
+++ test/Observable.ts
@@ -1,5 +1,6 @@
 import * as assert from 'assert'
-import * as Rx from 'rxjs'
+import { from } from 'rxjs'
+import { bufferTime, first } from 'rxjs/operators'
 
 import { observable } from '../src/Observable'
 
@@ -7,75 +8,63 @@ describe('Observable', () => {
   it('of', () => {
     const fa = observable.of(1)
     return new Promise((resolve, reject) => {
-      fa
-        .bufferTime(10)
-        .take(1)
-        .subscribe(events => {
-          try {
-            assert.deepEqual(events, [1])
-            resolve()
-          } catch (e) {
-            reject(e)
-          }
-        })
+      fa.pipe(bufferTime(10), first()).subscribe(events => {
+        try {
+          assert.deepEqual(events, [1])
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
+      })
     })
   })
 
   it('map', () => {
-    const fa = Rx.Observable.from([1, 2, 3])
+    const fa = from([1, 2, 3])
     const double = (n: number): number => n * 2
     const fb = observable.map(fa, double)
     return new Promise((resolve, reject) => {
-      fb
-        .bufferTime(10)
-        .take(1)
-        .subscribe(events => {
-          try {
-            assert.deepEqual(events, [2, 4, 6])
-            resolve()
-          } catch (e) {
-            reject(e)
-          }
-        })
+      fb.pipe(bufferTime(10), first()).subscribe(events => {
+        try {
+          assert.deepEqual(events, [2, 4, 6])
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
+      })
     })
   })
 
   it('ap', () => {
-    const fa = Rx.Observable.from([1, 2, 3])
+    const fa = from([1, 2, 3])
     const double = (n: number): number => n * 2
     const triple = (n: number): number => n * 3
-    const fab = Rx.Observable.from([double, triple])
+    const fab = from([double, triple])
     const fb = observable.ap(fab, fa)
     return new Promise((resolve, reject) => {
-      fb
-        .bufferTime(10)
-        .take(1)
-        .subscribe(events => {
-          try {
-            assert.deepEqual(events, [3, 6, 9])
-            resolve()
-          } catch (e) {
-            reject(e)
-          }
-        })
+      fb.pipe(bufferTime(10), first()).subscribe(events => {
+        try {
+          assert.deepEqual(events, [3, 6, 9])
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
+      })
     })
   })
 
   it('chain', () => {
-    const fa = Rx.Observable.from([1, 2, 3])
-    const fb = observable.chain(fa, a => Rx.Observable.from([a, a + 1]))
+    const fa = from([1, 2, 3])
+    const fb = observable.chain(fa, a => from([a, a + 1]))
     return new Promise((resolve, reject) => {
-      fb
-        .bufferTime(10)
-        .take(1)
-        .subscribe(events => {
-          try {
-            assert.deepEqual(events, [1, 2, 2, 3, 3, 4])
-            resolve()
-          } catch (e) {
-            reject(e)
-          }
-        })
+      fb.pipe(bufferTime(10), first()).subscribe(events => {
+        try {
+          assert.deepEqual(events, [1, 2, 2, 3, 3, 4])
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
+      })
     })
   })
 })
