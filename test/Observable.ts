31c3a0a559a236b162e4feba2ae831d5f727a912
gcanti <giulio.canti@gmail.com>
2018-10-16 08:10:31 +0200

    Test: refactor to promises

--- test/Observable.ts
+++ test/Observable.ts
@@ -1,38 +1,30 @@
 import * as assert from 'assert'
 import { from } from 'rxjs'
-import { bufferTime, first } from 'rxjs/operators'
+import { bufferTime } from 'rxjs/operators'
 
 import { observable } from '../src/Observable'
 
 describe('Observable', () => {
   it('of', () => {
     const fa = observable.of(1)
-    return new Promise((resolve, reject) => {
-      fa.pipe(bufferTime(10), first()).subscribe(events => {
-        try {
-          assert.deepEqual(events, [1])
-          resolve()
-        } catch (e) {
-          reject(e)
-        }
+    return fa
+      .pipe(bufferTime(10))
+      .toPromise()
+      .then(events => {
+        assert.deepEqual(events, [1])
       })
-    })
   })
 
   it('map', () => {
     const fa = from([1, 2, 3])
     const double = (n: number): number => n * 2
     const fb = observable.map(fa, double)
-    return new Promise((resolve, reject) => {
-      fb.pipe(bufferTime(10), first()).subscribe(events => {
-        try {
-          assert.deepEqual(events, [2, 4, 6])
-          resolve()
-        } catch (e) {
-          reject(e)
-        }
+    return fb
+      .pipe(bufferTime(10))
+      .toPromise()
+      .then(events => {
+        assert.deepEqual(events, [2, 4, 6])
       })
-    })
   })
 
   it('ap', () => {
@@ -41,30 +33,22 @@ describe('Observable', () => {
     const triple = (n: number): number => n * 3
     const fab = from([double, triple])
     const fb = observable.ap(fab, fa)
-    return new Promise((resolve, reject) => {
-      fb.pipe(bufferTime(10), first()).subscribe(events => {
-        try {
-          assert.deepEqual(events, [3, 6, 9])
-          resolve()
-        } catch (e) {
-          reject(e)
-        }
+    return fb
+      .pipe(bufferTime(10))
+      .toPromise()
+      .then(events => {
+        assert.deepEqual(events, [3, 6, 9])
       })
-    })
   })
 
   it('chain', () => {
     const fa = from([1, 2, 3])
     const fb = observable.chain(fa, a => from([a, a + 1]))
-    return new Promise((resolve, reject) => {
-      fb.pipe(bufferTime(10), first()).subscribe(events => {
-        try {
-          assert.deepEqual(events, [1, 2, 2, 3, 3, 4])
-          resolve()
-        } catch (e) {
-          reject(e)
-        }
+    return fb
+      .pipe(bufferTime(10))
+      .toPromise()
+      .then(events => {
+        assert.deepEqual(events, [1, 2, 2, 3, 3, 4])
       })
-    })
   })
 })
