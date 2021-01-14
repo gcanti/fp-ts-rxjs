07fbd9476f7ba1ca25525d6faecde87699c1b9cc
Iacopo <iacopo.pancotti@facile.it>
2021-01-14 22:44:09 +0100

    add ObservableThese

---
+++ src/ObservableThese.ts
@@ -0,0 +1,228 @@
+/**
+ * @since 0.6.12
+ */
+import * as TH from 'fp-ts/lib/These'
+import * as R from './Observable'
+import * as TT from 'fp-ts/lib/TaskThese'
+import { Bifunctor2 } from 'fp-ts/lib/Bifunctor'
+import { IO } from 'fp-ts/lib/IO'
+import { IOEither } from 'fp-ts/lib/IOEither'
+import { Monad2C } from 'fp-ts/lib/Monad'
+import { Observable } from 'rxjs'
+import { Task } from 'fp-ts/lib/Task'
+import { Semigroup } from 'fp-ts/lib/Semigroup'
+import { getTheseM } from 'fp-ts/lib/TheseT'
+import { pipe, pipeable } from 'fp-ts/lib/pipeable'
+import { Functor2 } from 'fp-ts/lib/Functor'
+import { Apply1 } from 'fp-ts/lib/Apply'
+import { Applicative2, Applicative2C } from 'fp-ts/lib/Applicative'
+
+const T = getTheseM(R.observable)
+
+/**
+ * @since 0.6.12
+ */
+export const URI = 'ObservableThese'
+
+/**
+ * @since 0.6.12
+ */
+export type URI = typeof URI
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind2<E, A> {
+    readonly [URI]: ObservableThese<E, A>
+  }
+}
+
+/**
+ * @since 0.6.12
+ */
+export interface ObservableThese<E, A> extends Observable<TH.These<E, A>> {}
+
+/**
+ * @since 0.6.12
+ */
+export const left: <E = never, A = never>(e: E) => ObservableThese<E, A> = T.left
+
+/**
+ * @since 0.6.12
+ */
+export const both: <E = never, A = never>(e: E, a: A) => ObservableThese<E, A> = T.both
+
+/**
+ * @since 0.6.12
+ */
+export const right: <E = never, A = never>(a: A) => ObservableThese<E, A> = T.right
+
+/**
+ * @since 0.6.12
+ */
+export const of: Applicative2<URI>['of'] = right
+
+/**
+ * @since 0.6.12
+ */
+export const rightObservable: <E = never, A = never>(ma: Observable<A>) => ObservableThese<E, A> = T.rightM
+
+/**
+ * @since 0.6.12
+ */
+export const leftObservable: <E = never, A = never>(ma: Observable<E>) => ObservableThese<E, A> = T.leftM
+
+/**
+ * @since 0.6.12
+ */
+export const fromIOEither: <E, A>(fa: IOEither<E, A>) => ObservableThese<E, A> = R.fromIO
+
+/**
+ * @since 0.6.12
+ */
+export function rightIO<E, A>(ma: IO<A>): ObservableThese<E, A> {
+  return rightObservable(R.fromIO(ma))
+}
+
+/**
+ * @since 0.6.12
+ */
+export function leftIO<E, A>(me: IO<E>): ObservableThese<E, A> {
+  return leftObservable(R.fromIO(me))
+}
+
+/**
+ * @since 0.6.12
+ */
+export function fromTaskThese<E, A>(t: TT.TaskThese<E, A>): ObservableThese<E, A> {
+  return R.fromTask(t)
+}
+
+/**
+ * @since 0.6.12
+ */
+export function toTaskThese<E, A>(o: ObservableThese<E, A>): TT.TaskThese<E, A> {
+  return () => o.toPromise()
+}
+
+/**
+ * @since 0.6.12
+ */
+export function fromTask<E, A>(ma: Task<A>): ObservableThese<E, A> {
+  return rightObservable(R.fromTask(ma))
+}
+
+/**
+ * @since 0.6.12
+ */
+export function fold<E, A, B>(
+  onLeft: (e: E) => Observable<B>,
+  onRight: (a: A) => Observable<B>,
+  onBoth: (e: E, a: A) => Observable<B>
+): (ma: ObservableThese<E, A>) => Observable<B> {
+  return ma => T.fold(ma, onLeft, onRight, onBoth)
+}
+
+/**
+ * @since 0.6.12
+ */
+export const swap: <E, A>(ma: ObservableThese<E, A>) => ObservableThese<A, E> = T.swap
+
+/**
+ * @category instances
+ * @since 0.6.12
+ */
+export function getApplicative<E>(A: Apply1<R.URI>, SE: Semigroup<E>): Applicative2C<URI, E> {
+  const AV = TH.getApplicative(SE)
+  const ap = <A>(fga: Observable<TH.These<E, A>>) => <B>(
+    fgab: Observable<TH.These<E, (a: A) => B>>
+  ): Observable<TH.These<E, B>> =>
+    A.ap(
+      A.map(fgab, h => (ga: TH.These<E, A>) => AV.ap(h, ga)),
+      fga
+    )
+  return {
+    URI,
+    _E: undefined as any,
+    map: T.map,
+    ap: (fab, fa) => pipe(fab, ap(fa)),
+    of
+  }
+}
+
+/**
+ * @category instances
+ * @since 0.6.12
+ */
+export function getMonad<E>(SE: Semigroup<E>): Monad2C<URI, E> {
+  const A = getApplicative(R.observable, SE)
+  return {
+    URI,
+    _E: undefined as any,
+    map: T.map,
+    ap: A.ap,
+    of,
+    chain: (ma, f) =>
+      pipe(
+        ma,
+        R.chain(
+          TH.fold(left, f, (e1, a) =>
+            pipe(
+              f(a),
+              R.map(
+                TH.fold(
+                  e2 => TH.left(SE.concat(e1, e2)),
+                  b => TH.both(e1, b),
+                  (e2, b) => TH.both(SE.concat(e1, e2), b)
+                )
+              )
+            )
+          )
+        )
+      )
+  }
+}
+
+/**
+ * @since 0.6.12
+ */
+export const functorObservableThese: Functor2<URI> = {
+  URI,
+  map: T.map
+}
+
+/**
+ * @category instances
+ * @since 0.6.12
+ */
+export const bifunctorObservableThese: Bifunctor2<URI> = {
+  URI,
+  bimap: T.bimap,
+  mapLeft: T.mapLeft
+}
+
+/**
+ * @category instances
+ * @since 0.6.12
+ */
+export const observableThese: Functor2<URI> & Bifunctor2<URI> = {
+  URI,
+  map: T.map,
+  bimap: T.bimap,
+  mapLeft: T.mapLeft
+}
+
+const { map, bimap, mapLeft } = pipeable(observableThese)
+
+export {
+  /**
+   * @since 0.6.12
+   */
+  bimap,
+  /**
+   * @since 0.6.12
+   */
+  map,
+  /**
+   * @since 0.6.12
+   */
+  mapLeft
+}
---
+++ test/ObservableThese.ts
@@ -0,0 +1,123 @@
+import * as assert from 'assert'
+import * as TH from 'fp-ts/lib/These'
+import * as TT from 'fp-ts/lib/TaskThese'
+import * as T from 'fp-ts/lib/Task'
+import { io } from 'fp-ts/lib/IO'
+import { pipe } from 'fp-ts/lib/pipeable'
+import { bufferTime } from 'rxjs/operators'
+import { monoidString } from 'fp-ts/lib/Monoid'
+
+import * as _ from '../src/ObservableThese'
+import { of as rxOf, Observable } from 'rxjs'
+
+describe('ObservableThese', () => {
+  it('rightIO', async () => {
+    const e = await _.rightIO(io.of(1))
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [TH.right(1)])
+  })
+  it('leftIO', async () => {
+    const e = await _.leftIO(io.of(1))
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [TH.left(1)])
+  })
+
+  it('fromTaskThese', async () => {
+    const e = await _.fromTaskThese(TT.right(1))
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [TH.right(1)])
+  })
+
+  it('toTaskThese', async () => {
+    const e = await _.toTaskThese(_.right(1))()
+    assert.deepStrictEqual(e, TH.right(1))
+  })
+
+  it('fromTask', async () => {
+    const e = await _.fromTask(T.of(1))
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [TH.right(1)])
+  })
+
+  it('fold left', async () => {
+    const f = (n: number): Observable<number> => rxOf(n * 2)
+    const g = (n: number): Observable<number> => rxOf(n * 3)
+    const n = (n: number, nn: number): Observable<number> => rxOf(n * nn * 4)
+    const e = await pipe(_.left(2), _.fold(f, g, n))
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [4])
+  })
+
+  it('fold right', async () => {
+    const f = (n: number): Observable<number> => rxOf(n * 2)
+    const g = (n: number): Observable<number> => rxOf(n * 3)
+    const n = (n: number, nn: number): Observable<number> => rxOf(n * nn * 4)
+    const e = await pipe(_.right(3), _.fold(f, g, n))
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [9])
+  })
+
+  it('fold both', async () => {
+    const f = (n: number): Observable<number> => rxOf(n * 2)
+    const g = (n: number): Observable<number> => rxOf(n * 3)
+    const n = (n: number, nn: number): Observable<number> => rxOf(n * nn * 4)
+    const e = await pipe(_.both(5, 3), _.fold(f, g, n))
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [60])
+  })
+
+  it('swap left to right', async () => {
+    const e = await pipe(_.left(1), _.swap)
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [TH.right(1)])
+  })
+
+  it('swap right to left', async () => {
+    const e = await pipe(_.right(1), _.swap)
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [TH.left(1)])
+  })
+
+  it('swap both to right and left', async () => {
+    const e = await pipe(_.both(1, 2), _.swap)
+      .pipe(bufferTime(10))
+      .toPromise()
+    assert.deepStrictEqual(e, [TH.both(2, 1)])
+  })
+
+  describe('getMonad', () => {
+    const M = _.getMonad(monoidString)
+    it('map', async () => {
+      const f = (n: number): number => n * 2
+      assert.deepStrictEqual(await M.map(_.right(1), f).toPromise(), TH.right(2))
+      assert.deepStrictEqual(await M.map(_.left('a'), f).toPromise(), TH.left('a'))
+      assert.deepStrictEqual(await M.map(_.both('a', 1), f).toPromise(), TH.both('a', 2))
+    })
+
+    it('ap', async () => {
+      const f = (n: number): number => n * 2
+      assert.deepStrictEqual(await M.ap(_.right(f), _.right(1)).toPromise(), TH.right(2))
+    })
+
+    it('chain', async () => {
+      const f = (n: number) => (n > 2 ? _.both(`c`, n * 3) : n > 1 ? _.right(n * 2) : _.left(`b`))
+      assert.deepStrictEqual(await M.chain(_.right(1), f).toPromise(), TH.left('b'))
+      assert.deepStrictEqual(await M.chain(_.right(2), f).toPromise(), TH.right(4))
+
+      assert.deepStrictEqual(await M.chain(_.left('a'), f).toPromise(), TH.left('a'))
+
+      assert.deepStrictEqual(await M.chain(_.both('a', 1), f).toPromise(), TH.left('ab'))
+      assert.deepStrictEqual(await M.chain(_.both('a', 2), f).toPromise(), TH.both('a', 4))
+      assert.deepStrictEqual(await M.chain(_.both('a', 3), f).toPromise(), TH.both('ac', 9))
+    })
+  })
+})
