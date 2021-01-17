c31357c644362ef67500edcdb38113463d76bbe1
gcanti <giulio.canti@gmail.com>
2021-01-17 18:38:52 +0100

    fix MonadObservable JSDocs

--- src/MonadObservable.ts
+++ src/MonadObservable.ts
@@ -16,6 +16,7 @@ import {
 import { Observable } from 'rxjs'
 
 /**
+ * @category type classes
  * @since 0.6.6
  */
 export interface MonadObservable<M> extends MonadTask<M> {
@@ -23,6 +24,7 @@ export interface MonadObservable<M> extends MonadTask<M> {
 }
 
 /**
+ * @category type classes
  * @since 0.6.6
  */
 export interface MonadObservable1<M extends URIS> extends MonadTask1<M> {
@@ -30,6 +32,7 @@ export interface MonadObservable1<M extends URIS> extends MonadTask1<M> {
 }
 
 /**
+ * @category type classes
  * @since 0.6.6
  */
 export interface MonadObservable2<M extends URIS2> extends MonadTask2<M> {
@@ -37,6 +40,7 @@ export interface MonadObservable2<M extends URIS2> extends MonadTask2<M> {
 }
 
 /**
+ * @category type classes
  * @since 0.6.6
  */
 export interface MonadObservable2C<M extends URIS2, E> extends MonadTask2C<M, E> {
@@ -44,6 +48,7 @@ export interface MonadObservable2C<M extends URIS2, E> extends MonadTask2C<M, E>
 }
 
 /**
+ * @category type classes
  * @since 0.6.6
  */
 export interface MonadObservable3<M extends URIS3> extends MonadTask3<M> {
@@ -51,6 +56,7 @@ export interface MonadObservable3<M extends URIS3> extends MonadTask3<M> {
 }
 
 /**
+ * @category type classes
  * @since 0.6.6
  */
 export interface MonadObservable3C<M extends URIS3, E> extends MonadTask3C<M, E> {
@@ -58,6 +64,7 @@ export interface MonadObservable3C<M extends URIS3, E> extends MonadTask3C<M, E>
 }
 
 /**
+ * @category type classes
  * @since 0.6.7
  */
 export interface MonadObservable4<M extends URIS4> extends MonadTask4<M> {
